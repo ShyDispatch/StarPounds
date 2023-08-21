0968af4deabed4679999d90bcb2f47c1a1afdeff
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2023-08-22 08:26:37 +1000

    Update skills.lua
    Fix for new stat calculation.

--- StarPounds/interface/scripted/starpounds/skills/skills.lua
+++ StarPounds/interface/scripted/starpounds/skills/skills.lua
@@ -265,7 +265,7 @@ function selectSkill(skill)
       local nextString = starPounds.getSkillLevel(skill.name) == skill.levels and "" or string.format("%s%.1f", nextAmount > 0 and "+" or "", nextAmount):gsub("%.?0+$", "").."%"
 
       local totalAmount = starPounds.getSkillBonus(skill.stat)
-      local totalIncrease = math.floor(0.5 + (100 * (totalAmount - baseAmount)/(baseAmount > 0 and baseAmount or 1)) * 10)/10
+      local totalIncrease = math.floor(0.5 + (100 * totalAmount/(baseAmount > 0 and baseAmount or 1)) * 10)/10
       local amount = totalIncrease ~= 0 and (starPounds.stats[skill.stat].invertDescriptor and (totalIncrease * -1) or totalIncrease) or 0
       local amountString = string.format("%s%.1f", amount > 0 and "+" or "", amount):gsub("%.?0+$", "").."%"
 

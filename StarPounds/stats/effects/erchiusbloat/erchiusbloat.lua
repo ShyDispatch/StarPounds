e5df2c80bb8f04dff73150da7f5bb8384b810570
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2023-12-26 23:07:00 +1000

    Bloat status effect tweaks
    Now ramps up sound correctly, and scaling/volume can be specified directly.

--- StarPounds/stats/effects/erchiusbloat/erchiusbloat.lua
+++ StarPounds/stats/effects/erchiusbloat/erchiusbloat.lua
@@ -1,6 +1,8 @@
 function init()
   script.setUpdateDelta(5)
   self.bloatAmount = effect.getParameter("bloatAmount", 10)
+  self.bloatIncrease = effect.getParameter("bloatIncrease", 1)
+  self.soundVolume = effect.getParameter("soundVolume")
   self.tickTime = effect.getParameter("tickTime", 1)
   self.tickTimer = 0
 
@@ -9,25 +11,31 @@ function init()
 
   self.saturation = 0
 
-  animator.playSound("geiger", -1)
+  animator.setSoundVolume("geiger", 0)
 end
 
 function update(dt)
   if world.entityType(entity.id()) == "npc" or (getmetatable ''.starPounds and getmetatable ''.starPounds.enabled) then
-    -- Gain 10 + (Seconds since bloating began) bloat per second. (Max: 100 per second)
+    -- Gain bloatAmount per tickTime, increased by bloatIncrease per tick. (Max: 100 per second)
     self.tickTimer = self.tickTimer - dt
     if self.tickTimer <= 0 then
       self.tickTimer = self.tickTime
-      self.bloatAmount = math.min(self.bloatAmount + 1 * self.tickTime, 100)
+      self.bloatAmount = math.min(self.bloatAmount + self.bloatIncrease, 100)
       self.saturation = math.floor(-self.desaturateAmount * self.bloatAmount * 0.01)
 
       local multiply = {255 + self.multiply[1] * self.bloatAmount * 0.01, 255 + self.multiply[2] * self.bloatAmount * 0.01, 255 + self.multiply[3] * self.bloatAmount * 0.01}
       local multiplyHex = string.format("%s%s%s", toHex(multiply[1]), toHex(multiply[2]), toHex(multiply[3]))
-
       world.sendEntityMessage(entity.id(), "starPounds.gainBloat", self.bloatAmount)
       effect.setParentDirectives(string.format("?saturation=%d?multiply=%s", self.saturation, multiplyHex))
-      animator.setSoundVolume("geiger", self.bloatAmount * 0.01)
+      if playedSound then
+        animator.setSoundVolume("geiger", self.soundVolume or (self.bloatAmount * 0.01), self.soundVolume and 1 or nil)
+      end
     end
+    -- Delaying this by a tick because setting the volume takes one tick, apparently.
+    if not playedSound then
+      animator.playSound("geiger", -1)
+    end
+    playedSound = true
   else
     effect.expire()
   end

cd1cfa5abf1b7fc7faf2ea0aff73b1c684f3a8af
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2024-03-20 23:05:31 +1000

    Honey slow status now colours the target

--- StarPounds/stats/effects/starpoundshoneyslow/starpoundshoneyslow.lua
+++ StarPounds/stats/effects/starpoundshoneyslow/starpoundshoneyslow.lua
@@ -1,16 +1,20 @@
 function init()
   animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
   animator.setParticleEmitterActive("drips", true)
+  local color = config.getParameter("color")
+  if color then
+    effect.setParentDirectives("fade="..config.getParameter("color").."=0.25")
+  end
 end
 
 function update(dt)
   mcontroller.controlModifiers({
-      groundMovementModifier = 0.45,
-	  airJumpModifier = 0.4,	  
-      speedModifier = 0.40
-    })
+    groundMovementModifier = 0.45,
+    airJumpModifier = 0.4,
+    speedModifier = 0.40
+  })
 end
 
 function uninit()
-  
+
 end

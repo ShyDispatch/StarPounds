6c52286977a9d49155b0aa2bf1eea9ed968a0c0e
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2025-01-13 21:34:54 +1000

    Humanoid folder restructure 2

--- StarPounds/npcs/fattiesbar/visitors/starpoundsaliposvisitor.lua
+++ StarPounds/npcs/fattiesbar/visitors/starpoundsaliposvisitor.lua
@@ -1,6 +1,21 @@
-local interact_old = interact
+local init_old = init
+starPounds_hasInteracted = {}
 
-function interact(args, ...)
-  --world.sendEntityMessage(args.sourceId, "starPounds.addEffect", "fizzy")
-  return interact_old(args, ...)
+function init()
+  local sayToEntity_old = sayToEntity
+  function sayToEntity(args, board)
+    if args.dialogType == "dialog.converse" then
+      -- Always say the blessing line, and bless them on the first interaction (60 second reset).
+      if not starPounds_hasInteracted[args.entity] or (os.time() - starPounds_hasInteracted[args.entity]) > 60 then
+        args = copy(args)
+        args.dialogType = "dialog.bless"
+        -- Bless them.
+        starPounds_hasInteracted[args.entity] = os.time()
+        world.sendEntityMessage(args.entity, "starPounds.addEffect", "alipos")
+      end
+    end
+    -- Do old stuff.
+    return sayToEntity_old(args, board)
+  end
+  init_old()
 end
